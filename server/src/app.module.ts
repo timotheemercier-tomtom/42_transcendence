36d380d52d7df9060815ecf2d928879469fe1d51
timothee <tmercier121@gmail.com>
2023-11-20 17:46:51 +0100

    auth module added - works

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [ChatModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot(), ChatModule, AuthModule],
+    // imports: [ChatModule, AuthModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
