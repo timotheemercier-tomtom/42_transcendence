766a74ecb93fd1772830dd6ebcc568fc8da49db3
alfa <alfa@devo.pw>
2023-11-21 16:06:53 +0100

    routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Chat from "./Chat";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import Error404 from "./page/Error404";
+import Home from "./page/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Chat></Chat>
+      <Router>
+        <div>
+          <header>
+            header
+            <nav>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+            </nav>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" Component={Home} />
+              <Route path="*" Component={Error404} />
+            </Routes>
+          </main>
+          <footer>footer</footer>
+        </div>
+      </Router>
     </>
   );
 }
