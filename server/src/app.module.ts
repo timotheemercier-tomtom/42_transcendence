6741a24122015eb0efc2efc2d19268d17dba5b11
alfa <alfa@devo.pw>
2023-11-27 13:39:56 +0100

    add postgresql type orm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const typeOrmModule = TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'your_username',
+  password: 'your_password',
+  database: 'your_database',
+  entities: [],
+  synchronize: true,
+});
 
 @Module({
-  imports: [ConfigModule.forRoot(), ChatModule, AuthModule],
+  imports: [ConfigModule.forRoot(), ChatModule, AuthModule, typeOrmModule],
   // imports: [ChatModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
