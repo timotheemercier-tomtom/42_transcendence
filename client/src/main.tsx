ed5ce4a307e5383cd782f988c83e89962834494f
timothee <tmercier121@gmail.com>
2024-01-31 13:53:26 +0100

    updated packages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,21 +8,6 @@ import { red } from '@mui/material/colors';
 import theme from './theme.ts';
 
 
-// const theme = createTheme({
-//     palette: {
-//       primary: {
-//         main: '#556cd6',
-//       },
-//       secondary: {
-//         main: '#19857b',
-//       },
-//       error: {
-//         main: red.A400,
-//       },
-//     },
-//   });
-  
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <ThemeProvider theme={theme}>
