6fc14f9be4206831aa50830fd0c2c95058045cde
timothee <tmercier121@gmail.com>
2024-01-31 13:10:35 +0100

    ....

--- server/src/auth/jwt-auth.guard.ts
+++ server/src/auth/jwt-auth.guard.ts
@@ -1,7 +1,12 @@
+/*
+// JWT-AUTH GUARD -> Verifies user identity 
+*/
+// First login: through 42 OAuth. After that, it checks JWT in each request to
+// keep the user logged in. It links our 42 OAuth with JWT, ensuring safe access
+// to our app.
+
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {}
-
-
