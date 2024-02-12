ae6901753e9d0476f91c58e8cfedddd9de527199
alfa <alfa@devo.pw>
2024-02-12 13:04:32 +0100

    [client] add getLogin from jwt

--- client/src/util.ts
+++ client/src/util.ts
@@ -5,3 +5,33 @@ export function getCookie(name: string) {
 }
 
 export const API = `http://${location.hostname}:3000`;
+
+export const getJWTPayload = () => {
+  const token = getCookie('accessToken') ?? '';
+
+  function base64UrlToBase64(base64Url: string) {
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const padLength = 4 - (base64.length % 4);
+    if (padLength < 4) {
+      for (let i = 0; i < padLength; i++) {
+        base64 += '=';
+      }
+    }
+    return base64;
+  }
+
+  function decodeJWT(token: string) {
+    const parts = token.split('.');
+    if (parts.length !== 3) {
+      throw new Error('Invalid JWT token format');
+    }
+    const header = JSON.parse(atob(base64UrlToBase64(parts[0])));
+    const payload = JSON.parse(atob(base64UrlToBase64(parts[1])));
+    return { header, payload };
+  }
+
+  const { payload } = decodeJWT(token);
+  return payload;
+};
+
+export const getLogin = () => getJWTPayload().login;
