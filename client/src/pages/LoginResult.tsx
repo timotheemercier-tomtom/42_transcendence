004b88a3b4e71ec2cc267aea52c14adfec340ff7
timothee <tmercier121@gmail.com>
2024-05-26 21:34:49 +0200

    up

--- client/src/pages/LoginResult.tsx
+++ client/src/pages/LoginResult.tsx
@@ -5,12 +5,10 @@ function LoginResult() {
   const navigate = useNavigate();
   const [query] = useSearchParams();
   useEffect(() => {
-    // Place the navigation logic inside useEffect
-    // to ensure it runs after the component mounts
-    // sessionStorage.setItem('user', query.get('u') ?? '');
+
     document.cookie = 'accessToken=' + query.get('token');
     navigate('/');
-  }, [navigate, query]); // Add navigate to the dependency array
+  }, [navigate, query]); 
 
   // Return null or a loader as the user won't see this component
   // because of the immediate redirect
